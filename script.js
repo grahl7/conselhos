06cf5136dc59f0e0a4b302793e050a6a8f8073a5
grahl7 <144069818+grahl7@users.noreply.github.com>
2023-10-16 19:18:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Function to translate advice to Portuguese using the Google Translate API
 async function translateToPortuguese(text) {
     const apiKey = 'AIzaSyBrzmygeiAIcagi6nu4Ausv0B_LOmlNpgE';
     const sourceLang = 'en';
@@ -18,7 +17,6 @@ async function translateToPortuguese(text) {
     return 'Translation error';
 }
 
-// Function to get and display a randomly translated advice in the modal
 async function getRandomAdviceAndTranslate() {
     const randomButton = document.getElementById('random-button');
     const modal = document.getElementById('myModal');
@@ -31,10 +29,8 @@ async function getRandomAdviceAndTranslate() {
         const data = await response.json();
         const advice = data.slip.advice;
 
-        // Translate the advice to Portuguese
         const translatedAdvice = await translateToPortuguese(advice);
 
-        // Display the advice in the modal
         const modalAdvice = document.getElementById('modal-advice');
         modalAdvice.textContent = translatedAdvice;
         modal.style.display = 'block';
@@ -47,18 +43,15 @@ async function getRandomAdviceAndTranslate() {
     }
 }
 
-// Add an event listener to the button to fetch random advice and translate it to Portuguese
 const randomButton = document.getElementById('random-button');
 randomButton.addEventListener('click', getRandomAdviceAndTranslate);
 
-// Close the modal when the "X" button is clicked
 const closeButton = document.getElementById('close-button');
 closeButton.addEventListener('click', () => {
     const modal = document.getElementById('myModal');
     modal.style.display = 'none';
 });
 
-// Close the modal when clicking outside of it
 window.addEventListener('click', (event) => {
     const modal = document.getElementById('myModal');
     if (event.target === modal) {
